73aac6d5c70db01dd34182cdb5953368b0e20185
vaibhavgoyal09 <79615436+vaibhavgoyal09@users.noreply.github.com>
2022-06-11 13:15:33 +0530

    Implemented Login Route

--- src/auth/routes/auth.py
+++ src/auth/routes/auth.py
@@ -3,7 +3,7 @@ from domain.model.jwt import Token
 from data.repository.UserRepository import UserRepository
 from fastapi import HTTPException, status, Depends
 from src.auth.jwt_service import create_access_token_for_user
-from src.auth.encryptor_service import get_password_hash
+from src.auth.encryptor_service import get_password_hash, verify_password
 from src.config import AppSettings, get_app_settings
 from typing import Dict
 from src.dependencies.auth import get_current_user
@@ -54,18 +54,54 @@ async def register_user(
         password=get_password_hash(user.password)
     )
 
-    print(user_obj.json())
     await user_repository.add_user(user_obj)
 
     return Token(
-        access_token = get_access_token_for_user(user_obj, app_settings)
+        access_token=get_access_token_for_user(user_obj, app_settings)
     )
 
 
 def get_access_token_for_user(user: User, settings: AppSettings) -> str:
-    return create_access_token_for_user(user, str(settings.secret_key.get_secret_value())) 
+    return create_access_token_for_user(user, str(settings.secret_key.get_secret_value()))
 
 
 @auth_router.get('/user/me')
 async def get_current_user_info(user: User = Depends(get_current_user)):
     return user
+
+
+@auth_router.post('/login', response_model=Token)
+async def login_user(
+    user: User,
+    app_settings: AppSettings = Depends(get_app_settings),
+    user_repository: UserRepository = Depends()
+):
+    try:
+        user_obj = await user_repository.get_user_by_username(username=user.username)
+    except:
+        user_obj = None
+
+    if not user_obj:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this username doesn't exists"
+        )
+
+    if not user.username or not user.password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Either Username or Passsword is not Valid"
+        )
+
+    is_password_valid = verify_password(
+        plain_password=user.password, hashed_password=user_obj.password)
+
+    if not is_password_valid:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect Password"
+        )
+
+    return Token(
+        access_token=get_access_token_for_user(user_obj, app_settings)
+    )
