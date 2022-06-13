ab68b806c6d73755e5c6ee30e80b4c4bb2c5abe5
vaibhavgoyal09 <79615436+vaibhavgoyal09@users.noreply.github.com>
2022-06-13 17:32:42 +0530

    Fixed bugs in notes route and improved response for no note exists

--- data/repository/note_repository.py
+++ data/repository/note_repository.py
@@ -1,6 +1,7 @@
 from domain.model.note import Note
 from data.model.note import Note as DBNote
 from typing import List
+from tortoise.expressions import Q
 
 
 class NoteRepository():
@@ -9,8 +10,6 @@ class NoteRepository():
         note_obj = DBNote(title=title, body=body, user_id=user_id)
 
         await note_obj.save()
-
-        print(note_obj.id)
 
         return Note(
             id=note_obj.id,
@@ -22,53 +21,58 @@ class NoteRepository():
         )
 
     async def get_all_notes(self, user_id: int) -> List[Note]:
-        try:
-            notes_obj = await DBNote.get(user_id=user_id)
-            notes = []
-            for note_obj in notes_obj:
-                notes.append(
-                    Note(
-                        id=note_obj.id,
-                        title=note_obj.title,
-                        body=note_obj.body,
-                        created_at=note_obj.created_at,
-                        updated_at=note_obj.updated_at,
-                        user_id=user_id
-                    )
+        notes_obj = await DBNote.filter(user_id=user_id)
+        notes = []
+        for note_obj in notes_obj:
+            notes.append(
+                Note(
+                    id=note_obj.id,
+                    title=note_obj.title,
+                    body=note_obj.body,
+                    created_at=note_obj.created_at,
+                    updated_at=note_obj.updated_at,
+                    user_id=user_id
                 )
-            return notes
-        except:
-            return []
+            )
+        return notes
 
     async def get_note_by_id(self, note_id: int, user_id: int):
-        try:
-            note_obj = await DBNote.get(id=note_id, user_id=user_id)
+        note_obj = await DBNote.filter(
+            Q(id=note_id) & Q(user_id=user_id)
+        ).first()
 
-            return Note(
-                id=note_obj.id,
-                title=note_obj.title,
-                body=note_obj.body,
-                created_at=note_obj.created_at,
-                updated_at=note_obj.updated_at,
-                user_id=user_id
-            )
-        except:
-            return None
+        return Note(
+            id=note_obj.id,
+            title=note_obj.title,
+            body=note_obj.body,
+            created_at=note_obj.created_at,
+            updated_at=note_obj.updated_at,
+            user_id=user_id
+        )
 
-    async def update_note(self, note: Note, user_id: int):
-        try:
-            updated_note = note.dict().copy()
-            updated_note.pop('id')
-            updated_note.pop('created_at')
-            updated_note.pop('updated_at')
-            updated_note.pop('user_id')
-            note_obj = await DBNote.filter(id=note.id, user_id=user_id).update(**updated_note)
-        except Exception:
-            raise Exception
-            pass
+    async def update_note(self, note_id: int, title: str, body: str, user_id: int):
+        
+        do_note_exists = await self.get_note_by_id(note_id=note_id, user_id=user_id)
+
+        if not do_note_exists:
+            raise Exception("Note does not exists")
+
+        updated_note = {
+            "title": title,
+            "body": body
+        }
+        await DBNote.filter(
+            Q(id=note_id) & Q(user_id=user_id)
+        ).update(**updated_note)
+
 
     async def delete_note(self, note_id: int, user_id: int):
-        try:
-            await DBNote.filter(id=note_id, user_id=user_id).delete()
-        except:
-            pass
+
+        do_note_exists = await self.get_note_by_id(note_id=note_id, user_id=user_id)
+
+        if not do_note_exists:
+            raise Exception("Note does not exists")
+
+        await DBNote.filter(
+            Q(id=note_id) & Q(user_id=user_id)
+        ).delete()
--- src/note/routes/note_crud.py
+++ src/note/routes/note_crud.py
@@ -1,10 +1,11 @@
 from src.dependencies.auth import get_current_user, get_app_settings
 from data.repository.note_repository import NoteRepository
 from domain.model.user import User
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from fastapi import Depends
 from pydantic import BaseModel
 from domain.model.note import Note
+import traceback
 
 note_router = APIRouter(prefix='/note', tags=['note'])
 
@@ -14,22 +15,34 @@ class NoteCreateRequest(BaseModel):
     body: str
 
 
+class NoteUpdateRequest(BaseModel):
+    id: int
+    title: str
+    body: str
+
+
 @note_router.post('/create')
 async def create_note(
-    note: NoteCreateRequest, 
+    note: NoteCreateRequest,
     user: User = Depends(get_current_user),
     repository: NoteRepository = Depends()
 ):
-    return await repository.create_note_and_return(title=note.title, body=note.body, user_id=user.id) 
+    return await repository.create_note_and_return(title=note.title, body=note.body, user_id=user.id)
 
 
 @note_router.patch('/update')
 async def update_note(
-    note: Note,
+    note: NoteUpdateRequest,
     user: User = Depends(get_current_user),
     repository: NoteRepository = Depends(),
 ):
-    await repository.update_note(note=note, user_id=user.id)
+    try:
+        return await repository.update_note(note_id=note.id, title=note.title, body=note.body, user_id=user.id)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Note doesn't exists"
+        )
 
 
 @note_router.delete('/delete')
@@ -38,7 +51,14 @@ async def delete_note(
     user: User = Depends(get_current_user),
     repository: NoteRepository = Depends()
 ):
-    await repository.delete_note(note_id=id, user_id=user.id)
+    try:
+        return await repository.delete_note(note_id=id, user_id=user.id)
+    except:
+        print(traceback.format_exc())
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Note doesn't exists"
+        )
 
 
 @note_router.get('/all')
@@ -46,7 +66,10 @@ async def get_all_notes(
     user: User = Depends(get_current_user),
     note_repository: NoteRepository = Depends()
 ):
-    return await note_repository.get_all_notes(user_id=user.id)
+    try:
+        return await note_repository.get_all_notes(user_id=user.id)
+    except:
+        return []
 
 
 @note_router.get('/{id}')
@@ -55,4 +78,10 @@ async def get_note_by_id(
     user: User = Depends(get_current_user),
     note_repository: NoteRepository = Depends()
 ):
-    return await note_repository.get_note_by_id(note_id=id, user_id=user.id)
+    try:
+        return await note_repository.get_note_by_id(note_id=id, user_id=user.id)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Note doesn't exists"
+        )
